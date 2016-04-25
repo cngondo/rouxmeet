c7f9c884d7d293f4440727665bb5dfb5af9e97b1
cngondo <devngondo@gmail.com>
2016-04-25 21:08:07 +0300

    adding speakers information to view and routes

--- routes/index.js
+++ routes/index.js
@@ -6,13 +6,59 @@ var appdata = require('../data.json');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   var myArtwork = [];
+  var myArtists = [];
+
+  //pass all the speakers information
+  myArtists = appdata.speakers;
   appdata.speakers.forEach(function(item){
     /*Populate the array with the artwork information from the JSON file*/
     myArtwork = myArtwork.concat(item.artwork)
   });
   res.render('index', {
     title: 'Rouxmeet',
-    artwork: myArtwork
+    artwork: myArtwork,
+    artists: myArtists,
+    page: 'home'
+   });
+});
+/* GET speakers page. */
+router.get('/speakers', function(req, res, next) {
+  var myArtwork = [];
+  var myArtists = [];
+
+  //pass all the speakers information
+  myArtists = appdata.speakers;
+  appdata.speakers.forEach(function(item){
+    /*Populate the array with the artwork information from the JSON file*/
+    myArtwork = myArtwork.concat(item.artwork)
+  });
+  res.render('speakers', {
+    title: 'Speakers',
+    artwork: myArtwork,
+    artists: myArtists,
+    page: 'artistList'
+   });
+});
+
+/* GET speakers page with specific speakerid(Details) ie the shortname according to the data.JSON file  */
+router.get('/speakers/:speakerid', function(req, res, next) {
+  var myArtwork = [];
+  var myArtists = [];
+
+  appdata.speakers.forEach(function(item){
+    //let's confirm if the speakerid matches, then we load his artwork
+    if (item.shortname == req.params.speakerid){
+      //load speaker information according to his ID
+      myArtists.push(item);
+      /*Populate the array with the artwork information from the JSON file*/
+      myArtwork = myArtwork.concat(item.artwork)
+    }
+  });
+  res.render('speakers', {
+    title: 'Speakers',
+    artwork: myArtwork,
+    artists: myArtists,
+    page: 'artistDetail'
    });
 });
 
