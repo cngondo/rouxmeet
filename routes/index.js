e9e9e5aae65305be7bc0bceeb9600203b0aaadae
cngondo <devngondo@gmail.com>
2016-04-25 20:05:32 +0300

    Passing artwork data to the routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+/* Enable to grab data from the data.JSON file */
+var appdata = require('../data.json');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Rouxmeet' });
+  var myArtwork = [];
+  appdata.speakers.forEach(function(item){
+    /*Populate the array with the artwork information from the JSON file*/
+    myArtwork = myArtwork.concat(item.artwork)
+  });
+  res.render('index', {
+    title: 'Rouxmeet',
+    artwork: myArtwork
+   });
 });
 
 module.exports = router;
